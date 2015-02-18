4fa6791963ed1ab10a6cb7bd786d47f527843957
Dennis Bartlett <dennis.bartlett@captix.com>
2015-02-18 16:46:31 -0600

    /setTime works?

--- app.js
+++ app.js
@@ -42,9 +42,8 @@ server.on('login', function(client) {
   client.on('chat', function(data) {
     // var message = '<'+client.username+'>' + ' ' + data.message;
     // Send chat to message processor to decide what to do with it.
-    console.log(data);
-    console.log(server);
     messageHandler(data.message, client);
+
     // broadcast(message, null, client.username);
   });
 });
@@ -55,16 +54,51 @@ server.on('error', function(error) {
 
 server.on('listening', function() {
   console.log('Server listening on port', server.socketServer.address().port);
+  startTicks();
 });
+
+function startTicks() {
+  server.ticks = 0;
+  setInterval(function() {
+    server.ticks++;
+  }, 50);
+}
 
 function messageHandler(message, client) { 
   if (message.split(" ")[0].indexOf('/') > -1) {
-    var message = "It looks like you tried to use a command.";
-    messageClient(message, client);
+    runCommand(message, client, function(response) {
+      messageClient(response, client);
+    });
   } else {
-    broadcast(message, null, client.username);
+    broadcast('chat', { message: message }, null, client.username);
   }
 }
+
+function runCommand(message, client, callback) {
+  var message = message.split(" ");
+  var response = "It looks like you tried to use a command.";
+  console.log(message);
+  if (message[0] == "/setTime") {
+    setServerTime(message[1]);
+    broadcast(client.username+" changed the time.");
+  }
+  if (message[0] == "/addExp") {
+    client.write(0x1F, '0.5', '3', '37');
+  }
+  if (message[0] == "/mode") {
+    client.write("game_state_change", { reason: '3', gameMode: message[1] });
+  }
+  if (message[0] == "/ticks") {
+    broadcast(client.username+" asked to look at bugs!!!");
+    messageClient(server.ticks, client);
+  }
+  callback(response);
+}
+
+function setServerTime(time) {
+  broadcast({ age: [0, 0] , time: [0, time] }, null, null, "update_time");
+}
+
 
 function messageClient(message, client) {
   var server = "Server"
@@ -76,23 +110,28 @@ function messageClient(message, client) {
   client.write(0x02, { message: JSON.stringify(msg) }, 2);
 }
 
-function broadcast(message, exclude, username) {
+function broadcast(data, exclude, username, packet) {
   var client, translate;
-  translate = username ? 'chat.type.announcement' : 'chat.type.text';
+  var packet = (typeof packet !== "undefined") ? packet : 'chat';
+  translate = !username ? 'chat.type.announcement' : 'chat.type.text';
   username = username || 'Server';
   for (var clientId in server.clients) {
     if (!server.clients.hasOwnProperty(clientId)) continue;
 
     client = server.clients[clientId];
-    if (client !== exclude) {
-      var msg = {
-        translate: translate,
-        "with": [
-          username,
-          message
-        ]
-      };
-      client.write('chat', { message: JSON.stringify(msg) });
+    if (packet == 'chat') {
+      if (client !== exclude) {
+        var msg = {
+          translate: translate,
+          "with": [
+            username,
+            data
+          ]
+        };
+        client.write('chat', { message: JSON.stringify(msg) });
+      }
+    } else {
+      client.write(packet, data);
     }
   }
 }
